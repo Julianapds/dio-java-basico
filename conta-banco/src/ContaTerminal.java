8138c7bcf0a1165ee8376215a3b17c09f7ad1fdf
juliana pires da silva <you@example.com>
2024-08-08 15:51:38 -0300

    primero commit

---
+++ conta-banco/src/ContaTerminal.java
@@ -0,0 +1,15 @@
+public class ContaTerminal {
+    public static void main(String[] args) throws Exception {
+
+
+        //todo:conhecer e importar a classe scanner
+
+        //Exibir as mensagens para nosso usuario
+
+        //obter pela scanner os valores digitados no terminal
+
+        // exibir a mensagem conta criada
+
+        System.out.println("Hello, World!");
+    }
+}
